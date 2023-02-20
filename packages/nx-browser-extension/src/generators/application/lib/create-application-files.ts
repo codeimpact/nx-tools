41f0560acc3ce0cdafa1c9a325636dcff184520d
Jan-Jaap Arends <jj.arends@codeimpact.nl>
2023-02-20 03:44:14 +0100

    Generate the files based on the settings.

--- packages/nx-browser-extension/src/generators/application/lib/create-application-files.ts
+++ packages/nx-browser-extension/src/generators/application/lib/create-application-files.ts
@@ -23,5 +23,37 @@ export function createApplicationFiles(host: Tree, options: NormalizedSchema) {
     options.projectRoot,
     {},
     templateVariables.offsetFromRoot + 'tsconfig.base.json'
-  );
+  )
+
+  if (options.newTabPage == false && options.popupPage == false) {
+    host.delete(`${options.projectRoot}/src/components/WelcomePage`);
+  }
+
+  if (options.backgroundScript == false) {
+    host.delete(`${options.projectRoot}/src/pages/background`);
+  }
+
+  if (options.contentScript == false) {
+    host.delete(`${options.projectRoot}/src/pages/content`);
+    host.delete(`${options.projectRoot}/src/components/ContentArea`);
+  }
+
+  if (options.newTabPage == false) {
+    host.delete(`${options.projectRoot}/src/pages/newtab`);
+  }
+
+  if (options.optionsPage == false) {
+    host.delete(`${options.projectRoot}/src/pages/options`);
+    host.delete(`${options.projectRoot}/src/components/OptionsPage`);
+  }
+
+  if (options.devtoolsPage == false) {
+    host.delete(`${options.projectRoot}/src/pages/devtools`);
+    host.delete(`${options.projectRoot}/src/pages/panel`);
+    host.delete(`${options.projectRoot}/src/components/DeveloperToolsPanel`);
+  }
+
+  if (options.popupPage == false) {
+    host.delete(`${options.projectRoot}/src/pages/popup`);
+  }
 }
