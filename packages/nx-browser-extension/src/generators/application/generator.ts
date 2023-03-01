8392658c00ae66338b09948c80a0b9112fa6a55b
Jan-Jaap Arends <jj.arends@codeimpact.nl>
2023-03-02 00:44:22 +0100

    Create the build in a separate build folder.

--- packages/nx-browser-extension/src/generators/application/generator.ts
+++ packages/nx-browser-extension/src/generators/application/generator.ts
@@ -1,63 +1,10 @@
-import {
-  addDependenciesToPackageJson,
-  formatFiles,
-  GeneratorCallback,
-  joinPathFragments, readProjectConfiguration,
-  Tree,
-  updateJson, updateProjectConfiguration
-} from '@nrwl/devkit';
-import { Schema, NormalizedSchema } from './schema';
+import { formatFiles, joinPathFragments, readProjectConfiguration, Tree,
+  updateProjectConfiguration } from '@nrwl/devkit';
+import { Schema } from './schema';
 import { normalizeOptions } from './lib/normalize-options';
-import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
 import { createApplicationFiles } from './lib/create-application-files';
-import {applicationGenerator, extendReactEslintJson, extraEslintDependencies } from '@nrwl/react';
+import { applicationGenerator } from '@nrwl/react';
 import { installCommonDependencies } from './lib/install-common-dependencies';
-import { Linter, lintProjectGenerator } from '@nrwl/linter';
-import { mapLintPattern } from '@nrwl/linter/src/generators/lint-project/lint-project';
-import { resolve } from 'path';
-
-async function addLinting(host: Tree, options: NormalizedSchema) {
-  const tasks: GeneratorCallback[] = [];
-  if (options.linter === Linter.EsLint) {
-    const lintTask = await lintProjectGenerator(host, {
-      linter: options.linter,
-      project: options.projectName,
-      tsConfigPaths: [
-        joinPathFragments(options.projectRoot, 'tsconfig.app.json'),
-      ],
-      unitTestRunner: options.unitTestRunner,
-      eslintFilePatterns: [
-        mapLintPattern(
-          options.projectRoot,
-          '{ts,tsx,js,jsx}',
-          options.rootProject
-        ),
-      ],
-      skipFormat: true,
-      rootProject: options.rootProject,
-      skipPackageJson: options.skipPackageJson,
-    });
-    tasks.push(lintTask);
-
-    updateJson(
-      host,
-      joinPathFragments(options.projectRoot, '.eslintrc.json'),
-      extendReactEslintJson
-    );
-
-    if (!options.skipPackageJson) {
-      const installTask = await addDependenciesToPackageJson(
-        host,
-        extraEslintDependencies.dependencies,
-        {
-          ...extraEslintDependencies.devDependencies
-        }
-      );
-      tasks.push(installTask);
-    }
-  }
-  return runTasksInSerial(...tasks);
-}
 
 export default async function createBrowserExtension(
   host: Tree,
@@ -89,19 +36,28 @@ export default async function createBrowserExtension(
 
   const config = readProjectConfiguration(host, options.project);
   config.targets.serve.executor = '@codeimpact/nx-browser-extension:serve';
-  config.targets.build.executor = '@codeimpact/nx-browser-extension:build';
-  delete(config.targets.preview);
 
-  const buildPath = config.targets.build.options.outputPath;
+  delete (config.targets.preview);
+  const distPath = joinPathFragments(
+    'dist',
+    options.projectRoot != '.'
+      ? options.projectRoot
+      : options.projectName
+  );
+
+  config.targets.build.executor = '@codeimpact/nx-browser-extension:build';
+  config.targets.build.options.outputPath = `${distPath}/build`;
+
   config.targets.package = {
     executor: '@codeimpact/nx-browser-extension:package',
     dependsOn: ['build'],
     options: {
-      sourceDir: config.targets.build.options.outputPath,
-      artifactsDir: resolve(buildPath, '..'),
+      artifactsDir: `${distPath}/package`,
     },
   };
   await updateProjectConfiguration(host, config.name, config);
+
+  installCommonDependencies(host, options);
 
   await formatFiles(host);
 }
